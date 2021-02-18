40e4fb0f48af08e1381595801a6b2ff91dccb942
Alice <alice.moretti@truemedinc.com>
2021-02-18 11:12:18 +0200

    Removed button component from text section

--- src/AboutText.js
+++ src/AboutText.js
@@ -1,6 +1,5 @@
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
   backgroundPicture: {
