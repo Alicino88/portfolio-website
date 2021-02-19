e79831104c848b4dbffff4d97c94b1b5e7b37041
Alice <alice.moretti@truemedinc.com>
2021-02-19 12:04:07 +0200

    Updates to Work page

---
+++ src/SingleProject.js
@@ -0,0 +1,134 @@
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  pictureContainerBlue: {
+    width: "40vw",
+    position: "relative",
+    "&::before": {
+      content: "''",
+      background: "hsla(201, 48%, 77%, 1)",
+      opacity: "0.7",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+    },
+    "&:hover::before": {
+      background: "none",
+    },
+  },
+  pictureContainerGrey: {
+    width: "40vw",
+    position: "relative",
+    "&::before": {
+      content: "''",
+      background: "hsla(240, 4%, 17%, 1)",
+      opacity: "0.7",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+    },
+    "&:hover::before": {
+      background: "none",
+    },
+  },
+  picAlignRight: {
+    flexDirection: "row-reverse",
+  },
+  picAlignLeft: {
+    flexDirection: "row",
+  },
+  pictureStyle: {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+  },
+
+  textContainerWhite: {
+    backgroundColor: "#FFFAFF",
+  },
+
+  textContainerGrey: {
+    backgroundColor: "#303036",
+  },
+  whiteText: {
+    color: "#FFFAFF",
+  },
+  greyText: {
+    color: "#303036",
+  },
+
+  title: {
+    borderLeft: "2px solid #30BCED",
+    paddingLeft: "10px",
+  },
+
+  text: {
+    marginTop: "20px",
+  },
+});
+const Singleproject = ({
+  title,
+  picture,
+  picAlignRight,
+  whiteBackground,
+  greyText,
+  pictureBlue,
+  pictureLink,
+  text,
+}) => {
+  const classes = useStyles();
+  return (
+    <div
+      style={{ display: "flex", height: "auto" }}
+      className={picAlignRight ? classes.picAlignRight : classes.picAlignLeft}
+    >
+      <a href={pictureLink} rel="noopener noreferrer" target="_blank">
+        <div
+          className={
+            pictureBlue
+              ? classes.pictureContainerBlue
+              : classes.pictureContainerGrey
+          }
+        >
+          <img
+            src={picture}
+            alt="Truemed services page"
+            className={classes.pictureStyle}
+          />
+        </div>
+      </a>
+      <div
+        style={{
+          width: "60vw",
+          padding: "90px 0 90px 90px",
+          display: "flex",
+          alignItems: "center",
+        }}
+        className={
+          whiteBackground
+            ? classes.textContainerWhite
+            : classes.textContainerGrey
+        }
+      >
+        <div
+          style={{ width: "40vw" }}
+          className={greyText ? classes.greyText : classes.whiteText}
+        >
+          <div className={classes.title}>
+            <Typography variant="h5" component="h2">
+              {title}
+            </Typography>
+          </div>
+          <div className={classes.text}>
+            <Typography variant="h6" component="h3">
+              {text}
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Singleproject;
--- src/Work.js
+++ src/Work.js
@@ -1,11 +1,53 @@
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Header from "./Header.js";
+import SingleProject from "./SingleProject.js";
+import Truemed from "./images/truemed1.png";
+import Users from "./images/react_users.png";
+import Lando from "./images/lando.png";
+import Crostata from "./images/crostata.png";
 
 const Work = () => {
   return (
     <div>
       <Header title={"Work"} />
+      <SingleProject
+        picAlignRight
+        title={"React Users Page"}
+        picture={Users}
+        pictureLink={"https://react-users-page2.netlify.app/"}
+        text="I had fun creating a small React application. The app fetches some users data from an API via Axios and uses useState, useEffect, useParams hooks."
+      />
+      <SingleProject
+        title={"TrueMed Services Page"}
+        picture={Truemed}
+        whiteBackground
+        greyText
+        pictureBlue
+        pictureLink={"https://truemedinc.com/services"}
+        text="This page was developed with Vue and Vuetify and it has been one of the several projects I carried out during my last traineeship as a frontend developer."
+      />
+      <SingleProject
+        title={"Crostata Cake Website"}
+        Crostata
+        Cake
+        Website
+        picture={Crostata}
+        picAlignRight
+        pictureLink={"https://codepen.io/Alicinetto/full/mdJqdEq"}
+        text="I must admit, this website is nothing fancy as it doesn't use any Javascript framework. Nevertheless I wanted to include it in my portfolio as I really like the graphics"
+      />
+      <SingleProject
+        title={"Product Landing Page"}
+        picture={Lando}
+        whiteBackground
+        greyText
+        pictureBlue
+        pictureLink={"https://codepen.io/Alicinetto/full/VweyBgv"}
+        text="I love coding landing pages, it's fun and allows you to think also
+      from a digital marketing perspective: how to grasp the attention of the reader? How to
+      drive conversions?"
+      />
     </div>
   );
 };
