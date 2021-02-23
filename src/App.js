be675a0f815260fa825b373782ed01a1c36d9dee
Alice <alice.moretti@truemedinc.com>
2021-02-23 14:57:23 +0200

    Added Material-UI responsive font-size

--- src/App.js
+++ src/App.js
@@ -6,26 +6,44 @@ import Contact from "./Contact.js";
 import Footer from "./Footer.js";
 import { Route } from "react-router-dom";
 
+//with the below we can create a theme to override the default one
+import {
+  createMuiTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+
+let theme = createMuiTheme({
+  typography: {
+    fontFamily: ['"Source Sans Pro"', "Montserrat"].join(","),
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+  },
+});
+theme = responsiveFontSizes(theme);
+
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="Content">
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/About">
-          <About />
-        </Route>
-        <Route exact path="/Work">
-          <Work />
-        </Route>
-        <Route exact path="/Contact">
-          <Contact />
-        </Route>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Navbar />
+        <div className="Content">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/About">
+            <About />
+          </Route>
+          <Route exact path="/Work">
+            <Work />
+          </Route>
+          <Route exact path="/Contact">
+            <Contact />
+          </Route>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ThemeProvider>
   );
 }
 
