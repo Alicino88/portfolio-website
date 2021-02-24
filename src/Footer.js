696f98bda9fc66c2af8b8219f5f209f83a2a51dc
Alice <alice.moretti@truemedinc.com>
2021-02-24 22:02:35 +0200

    Finalized responsive footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,12 +2,16 @@ import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
+import IconButton from "@material-ui/core/IconButton";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   footerStyle: {
     backgroundColor: "#050401",
     color: "#FFFAFF",
     padding: "30px 0 0 90px",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "30px",
+    },
   },
   iconsStyle: {
     display: "flex",
@@ -19,8 +23,24 @@ const useStyles = makeStyles({
     display: "flex",
     listStyle: "none",
     padding: "10px 0 40px 0",
+    [theme.breakpoints.down("xs")]: {
+      disply: "flex",
+      flexDirection: "column",
+    },
   },
-});
+
+  listStyle: {
+    borderLeft: "1px solid #FFFAFF",
+    marginLeft: "10px",
+    paddingLeft: "10px",
+    [theme.breakpoints.down("xs")]: {
+      borderLeft: "none",
+      marginLeft: "0px",
+      paddingLeft: "0px",
+      paddingTop: "5px",
+    },
+  },
+}));
 
 const Footer = () => {
   const classes = useStyles();
@@ -29,43 +49,46 @@ const Footer = () => {
       <div>
         <ul className={classes.iconsStyle}>
           <li>
-            <LinkedInIcon rounded style={{ fontSize: 40 }} />
+            <LinkedInIcon
+              rounded
+              style={{ fontSize: 40, cursor: "pointer" }}
+              onClick={() =>
+                window.open("https://www.linkedin.com/in/alicemoretti")
+              }
+            />
           </li>
           <li style={{ paddingLeft: "30px", paddingTop: "4px" }}>
-            <GitHubIcon style={{ fontSize: 30 }} />
+            <GitHubIcon
+              style={{ fontSize: 30, cursor: "pointer" }}
+              onClick={() =>
+                window.open("https://github.com/Alicino88?tab=repositories")
+              }
+            />
           </li>
         </ul>
         <ul className={classes.copyrightStyle}>
           <li>© Alice Moretti</li>
-          <li
-            style={{
-              borderLeft: "1px solid #FFFAFF",
-              marginLeft: "10px",
-              paddingLeft: "10px",
-            }}
-          >
+          <li className={classes.listStyle}>
             Built with{" "}
             <a
               href="https://reactjs.org/"
+              target="_blank"
+              rel="noreferrer"
               style={{
                 textDecoration: "none",
                 borderBottom: "1px dotted",
                 color: "inherit",
               }}
             >
-              RectJs
+              ReactJs
             </a>
           </li>
-          <li
-            style={{
-              borderLeft: "1px solid #FFFAFF",
-              marginLeft: "10px",
-              paddingLeft: "10px",
-            }}
-          >
+          <li className={classes.listStyle}>
             Icons by{" "}
             <a
               href="https://icons8.com/"
+              target="_blank"
+              rel="noreferrer"
               style={{
                 textDecoration: "none",
                 borderBottom: "1px dotted",
