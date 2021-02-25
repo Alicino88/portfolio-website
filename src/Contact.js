c0f1ee8a01fb816e8bd40f536efe55c5822c09ea
Alice <alice.moretti@truemedinc.com>
2021-02-25 14:54:42 +0200

    Made contact form responsive

--- src/Contact.js
+++ src/Contact.js
@@ -1,25 +1,61 @@
 import { TextareaAutosize, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import classNames from "classnames";
 
 import Header from "./Header.js";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   background: {
     background: "#FFFAFF",
     color: "#050401",
-    padding: "90px 90px 0px 90px",
-    height: "100vh",
+    padding: "90px 90px 90px 90px",
+    height: "auto",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "60px",
+    },
+    [theme.breakpoints.down("xs")]: {
+      paddingLeft: "30px",
+    },
   },
 
   formStyle: {
-    width: "60vw",
+    width: "45vw",
     marginTop: "50px",
     maxWidth: "800px",
+    [theme.breakpoints.down("md")]: {
+      width: "60vw",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "70vw",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "80vw",
+    },
+  },
+
+  nameAndEmailSection: {
+    [theme.breakpoints.down("xs")]: {
+      display: "flex",
+      flexDirection: "column",
+    },
   },
 
   nameAndEmailContainer: {
     width: "50%",
     float: "left",
+    [theme.breakpoints.down("xs")]: {
+      width: "100%",
+    },
+  },
+
+  emailPaddingLeft: {
+    paddingLeft: "20px",
+    marginBottom: "50px",
+    [theme.breakpoints.down("xs")]: {
+      paddingLeft: "0px",
+      paddingRight: "20px",
+    },
   },
 
   nameAndEmailStyle: {
@@ -30,6 +66,13 @@ const useStyles = makeStyles({
     textDecoration: "none",
     outline: "0",
     color: "#050401",
+    padding: "20px 20px 20px 20px",
+    fontFamily: "Source Sans Pro",
+    fontSize: "20px",
+    borderRadius: "4px",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "15px",
+    },
 
     "&:focus": {
       border: "2px solid #30BCED",
@@ -50,6 +93,13 @@ const useStyles = makeStyles({
     fontWeight: "600",
     textTransform: "uppercase",
     marginBottom: "20px",
+    fontFamily: "Montserrat",
+  },
+
+  textAreaContainer: {
+    [theme.breakpoints.down("xs")]: {
+      paddingRight: "20px",
+    },
   },
 
   textAreaStyle: {
@@ -59,25 +109,34 @@ const useStyles = makeStyles({
     textDecoration: "none",
     outline: "0",
     color: "#050401",
+    padding: "20px 20px 20px 20px",
+    fontFamily: "Source Sans Pro",
+    fontSize: "20px",
+    borderRadius: "4px",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "15px",
+    },
 
     "&:focus": {
       border: "2px solid #30BCED",
     },
   },
   buttonStyle: {
-    marginTop: "50px",
-    border: "none",
-    cursor: "pointer",
-    padding: "20px 40px",
-    backgroundColor: "#050401",
-    color: "#FFFAFF",
-    textTransform: "upperCase",
-
+    padding: "10px 30px",
+    fontWeight: "600",
+    marginTop: "40px",
+    color: "#30BCED",
+    border: "2px solid #30BCED",
     "&:hover": {
       backgroundColor: "#30BCED",
+      color: "#FFFAFF",
     },
   },
-});
+
+  "&:hover": {
+    backgroundColor: "#30BCED",
+  },
+}));
 
 const Contact = (Name) => {
   const classes = useStyles();
@@ -86,7 +145,7 @@ const Contact = (Name) => {
       <Header title={"Contact"} />
       <div className={classes.background}>
         <Typography variant="h5" component="h2">
-          Would you like to connect?
+          Would you like to discuss a possible cooperation?
         </Typography>
 
         <form
@@ -101,36 +160,40 @@ const Contact = (Name) => {
 
           {/*for attribute on the label and id attribute on input must be the same to bind them together
             name attribute on the input field is necessary for the information to be sent*/}
-          <div
-            className={classes.nameAndEmailContainer}
-            style={{ paddingRight: "20px", marginBottom: "50px" }}
-          >
-            <label className={classes.labelStyle} for="name">
-              Name
-            </label>
-            <input
-              className={classes.nameAndEmailStyle}
-              type="text"
-              id="name"
-              name="name"
-            ></input>
-          </div>
-          <div
-            className={classes.nameAndEmailContainer}
-            style={{ paddingLeft: "20px", marginBottom: "50px" }}
-          >
-            <label className={classes.labelStyle} for="email">
-              Email
-            </label>
-            <input
-              className={classes.nameAndEmailStyle}
-              type="email"
-              id="email"
-              name="email"
-            ></input>
+          <div className={classes.nameAndEmailSection}>
+            <div
+              className={classes.nameAndEmailContainer}
+              style={{ paddingRight: "20px", marginBottom: "50px" }}
+            >
+              <label className={classes.labelStyle} for="name">
+                Name
+              </label>
+              <input
+                className={classes.nameAndEmailStyle}
+                type="text"
+                id="name"
+                name="name"
+              ></input>
+            </div>
+            <div
+              className={classNames(
+                classes.nameAndEmailContainer,
+                classes.emailPaddingLeft
+              )}
+            >
+              <label className={classes.labelStyle} for="email">
+                Email
+              </label>
+              <input
+                className={classes.nameAndEmailStyle}
+                type="email"
+                id="email"
+                name="email"
+              ></input>
+            </div>
           </div>
 
-          <div>
+          <div className={classes.textAreaContainer}>
             <label className={classes.labelStyle} for="message">
               Message
             </label>
@@ -143,9 +206,14 @@ const Contact = (Name) => {
             ></textarea>
           </div>
           <div className={classes.buttonContainer}>
-            <button className={classes.buttonStyle} type="submit">
-              Test continuos deployment
-            </button>
+            <Button
+              className={classes.buttonStyle}
+              disableElevation
+              disableRipple
+              variant="outlined"
+            >
+              Send message
+            </Button>
           </div>
         </form>
       </div>
