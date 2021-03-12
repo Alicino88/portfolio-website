ae6dc083c5367f8931de46b1b9fe2669d8bda491
Alice <alice.moretti@truemedinc.com>
2021-03-12 09:30:28 +0200

    Removed textAreaAutosize from Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,4 @@
-import { TextareaAutosize, Typography } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import classNames from "classnames";
